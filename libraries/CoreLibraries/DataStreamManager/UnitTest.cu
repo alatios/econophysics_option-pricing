bfd19e09fb7d10ea54c5ffff8dfa4bb9b97f3006
Riccardo Aiolfi <riccardoaiolfi@MBP-di-Riccardo.homenet.telecomitalia.it>
2019-07-02 10:31:13 +0200

    Add DataStreamManager UnitTest and changes on unittest.sh

---
+++ libraries/CoreLibraries/DataStreamManager/UnitTest.cu
@@ -0,0 +1,54 @@
+#include "Data_stream_manager.cuh"
+#include <iostream>
+
+int main(){
+
+    Data_stream_manager data_stream_manager("input.dat");
+
+    Input_gpu_data inputGPU;
+	Input_market_data inputMarket;
+	Input_option_data inputOption;
+	Input_MC_data inputMC;
+    data_stream_manager.ReadInputData(inputGPU, inputOption, inputMarket, inputMC);
+
+    bool test;
+
+	cout << "\n-------------Data_stream_manager_test-------------\n";
+	cout << "\nMethods testing\n";
+
+    // Input GPU data
+    test = (inputGPU.GetNumberOfThreadsPerBlock() == static_cast<unsigned int>(512));
+	cout << test << "\t";
+	test = (inputGPU.GetTotalNumberOfThreads() == static_cast<unsigned int>(5120));
+    cout << test << "\n";
+    
+    // Input market data
+    test = (inputMarket.InitialPrice == static_cast<double>(100.));
+    cout << test << "\t";
+    test = (inputMarket.Volatility == static_cast<double>(0.25));
+    cout << test << "\t";
+    test = (inputMarket.RiskFreeRate == static_cast<double>(0.01));
+    cout << test << "\n";
+
+    // Input option data
+    test = (inputOption.OptionType == char('e'));
+	cout << test << "\t";
+	test = (inputOption.NumberOfIntervals == static_cast<unsigned int>(365));
+	cout << test << "\t";
+	test = (inputOption.TimeToMaturity == static_cast<double>(1.));
+	cout << test << "\t";
+	test = (inputOption.StrikePrice == static_cast<double>(100.));
+	cout << test << "\t";
+    test = (inputOption.B == static_cast<double>(2.));
+	cout << test << "\t";
+	test = (inputOption.N == static_cast<double>(1.));
+	cout << test << "\t";
+    test = (inputOption.K == static_cast<double>(0.3));
+    cout << test << "\n";
+
+    // Input Monte Carlo data
+    test = (inputMC.GetNumberOfSimulationsPerThread(inputGPU) == static_cast<unsigned int>(977));
+	cout << test << "\n";
+
+    return 0;
+}
--- libraries/unit_test.sh
+++ libraries/unit_test.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 cd InputStructures/InputOptionData/ &&
 make run &&
+cd ../InputMarketData/ $$
+make run &&
 cd  ../InputMCData/ &&
 make run &&
 cd  ../InputGPUData/ &&
+make run &&
+cd ../../OutputStructures/OutputMCData/ &&
 make run &&
 cd ../../CoreLibraries/Path &&
 make run &&
 cd ../Statistics &&
 make run &&
-echo -e "\n\n\n\n\n\nCORRELATION TESTS\n\n\n\n\n" &&
-cd ../RandomGenerator &&
+cd ../DataStreamManager &&
 make run &&
 cd ../..
