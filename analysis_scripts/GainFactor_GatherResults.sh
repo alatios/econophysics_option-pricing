867186fff63a38dc67933b9be9017a6476136d91
Alessandro De Gennaro <novecento.db@fastwebnet.it>
2019-07-13 16:40:49 +0100

    Add completed GainFactor results

--- analysis_scripts/GainFactor_GatherResults.sh
+++ analysis_scripts/GainFactor_GatherResults.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUTFILE="../outputs/GainFactor_Tesla/GainFactor_GatheredResults.dat"
+
 awk '{print FILENAME ":" $0}' ../outputs/GainFactor_Tesla/output__* \
 	| sed -n '/## HOST OUTPUT MONTE CARLO DATA ##/,/## DEVICE OUTPUT MONTE CARLO DATA ##/p' \
 	| grep "Computation time" \
@@ -7,5 +9,15 @@ awk '{print FILENAME ":" $0}' ../outputs/GainFactor_Tesla/output__* \
 	| awk '{gsub("_"," ");print}' \
 	| awk '{gsub(".dat:Computation time \\[ms\\]:","");print}' \
 	> CPU.dat;
+
+awk '{print FILENAME ":" $0}' ../outputs/GainFactor_Tesla/output__* \
+	| sed -n '/## DEVICE OUTPUT MONTE CARLO DATA ##/,/Negative price counter/p' \
+	| grep "Computation time" \
+	| sed -e '/^.*/s/^.*\(\[ms\]: \)//' \
+	> GPU.dat
+
+paste -d" " CPU.dat GPU.dat > ${OUTPUTFILE};
+sed -i '1 i\blocks m cpu gpu' ${OUTPUTFILE};
+rm GPU.dat CPU.dat;
 
 exit 0
